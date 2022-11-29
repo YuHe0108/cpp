8464c3eb074ceacf16e4bce93a8d1e471ec6c6a0
yuhe <yuhe@vortexinfo.cn>
2022-11-29 16:06:33 +0800

    添加 NumCpp 的使用方法。

--- main.cpp
+++ main.cpp
@@ -5,12 +5,20 @@
 #include <iostream>
 
 #include "image_utils.h"
+#include "NumCpp.hpp"
 
 int main() {
     std::shared_ptr<cv::Mat> inputs = std::make_shared<cv::Mat>(cv::imread("/mnt/YuHe/remote_code/2.jpg"));
     auto res = LetterBox(inputs, 224, 224);
     std::cout << (res == nullptr) << std::endl;
-    cv::imwrite("/mnt/YuHe/r_img.jpg", *res);
+
+    nc::NdArray<float> a = {{1, 2},
+                            {3, 4}};
+    std::cout << a[0] << std::endl;
+    std::cout << a[1] << std::endl;
+    std::cout << a[2] << std::endl;
+    std::cout << a.shape() << std::endl;
+
     std::cout << "Done" << std::endl;
     return 1;
 }
