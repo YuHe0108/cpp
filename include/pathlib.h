992be39ba0aa5e08414debaddbabdac69a91d18b
yuhe <yuhe@vortexinfo.cn>
2022-12-05 18:00:29 +0800

    pathlib: 判定文件路径是否存在。

--- include/pathlib.h
+++ include/pathlib.h
@@ -5,6 +5,7 @@
 #ifndef CPP_PATHLIB_H
 #define CPP_PATHLIB_H
 
+#include <fstream>
 #include <string>
 #include <vector>
 #include <iostream>
@@ -14,17 +15,22 @@
 
 class PathLib {
 public:
+    PathLib() = default;
+
     PathLib(const std::string &inputPath);
 
     std::vector<std::string> IterDir(const std::string &dir);
 
     static bool FileExist(const std::string &path);
 
+    std::string PathJoin(const std::string &inputPath);
+
     std::string path; // 路径
     std::string suffix; // 文件的后缀
     std::string stem; // file
     std::string name; // file.txt
     std::string parent;
+    std::string sepCharacter = "\\"; // 路径的分隔符
 };
 
 #endif //CPP_PATHLIB_H
--- src/pathlib.cpp
+++ src/pathlib.cpp
@@ -3,22 +3,38 @@
 //
 #include "pathlib.h"
 
-std::vector<std::string> PathLib::IterDir(const std::string &dir) {
+std::vector<std::string> PathLib::IterDir(const std::string &dir) { // 遍历this.path下的文件和目录
     std::vector<std::string> res;
+    this->path;
     return res;
 }
 
 bool PathLib::FileExist(const std::string &inputPath) {
-//    if (!inputPath.empty() && std::experimental::filesystem::exists(inputPath)) return true;
-    return false;
+    std::fstream _file;
+    _file.open(inputPath.c_str(), std::ios::in);
+    if (!_file) return false;
+    return true;
 }
 
-PathLib::PathLib(const std::string &inputPath) {
+PathLib::PathLib(const std::string &inputPath) { // 对象初始化, 并赋值fileName等信息
     this->path = inputPath;
-    auto splitRes = split_string(inputPath, ".");
-    if (!splitRes.empty()) {
-        this->stem = std::move(splitRes[splitRes.size() - 2]);
-        this->suffix = std::move(*splitRes.end());
-        this->name = this->stem + "." + this->suffix;
+
+    if (inputPath.find('/') != std::string::npos) this->sepCharacter = '/';
+
+    auto splitBasedSep = split_string(inputPath, this->sepCharacter);
+    this->parent = ""; // 父目录的路径
+    this->name = std::move(splitBasedSep.back());
+    for (auto item: splitBasedSep) {
+        if (!item.empty()) this->parent += this->sepCharacter + std::move(item);
     }
+    if (!this->name.empty()) {
+        auto splitFileName = split_string(this->name, ".");
+        this->stem = std::move(*splitFileName.begin());
+        if (splitFileName.size() == 2) this->suffix = std::move(splitFileName.back());
+    }
+}
+
+std::string PathLib::PathJoin(const std::string &inputPath) { // 路径拼接
+    this->path += this->sepCharacter + inputPath;
+    return this->path;
 }
